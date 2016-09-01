0722c51e2cd7d84df804d465646bd5bb13ca2227
Fabiano Teichmann <fabianofreelancer@gmail.com>
2016-08-31 22:12:05 -0300

    crud bd, e visualização

--- appyud/models.py
+++ appyud/models.py
@@ -5,11 +5,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-	autor = models.ForeignKey('auth.User')
-	titulo = models.CharField('None:',max_length=100)
-	sub_titulo = models.CharField('Sub titulo', max_length= 150)
-	texto = models.TextField()
-	postado = models.DateTimeField(default=timezone.now)
+    autor = models.ForeignKey('auth.User')
+    titulo = models.CharField('None:',max_length=100)
+    sub_titulo = models.CharField('Sub titulo', max_length= 150)
+    texto = models.TextField()
+    postado = models.DateTimeField(default=timezone.now)
 
-	def __str__(self):
-		return self.titulo
+    def __str__(self):
+	    return self.titulo
